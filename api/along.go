059749ecfa67caa2d61f15518deaa6d40aef1999
Coniglio <halfway67@icloud.com>
2020-02-14 01:16:24 +0900

    沿線情報取得処理追加

--- api/along.go
+++ api/along.go
@@ -3,12 +3,21 @@ package api
 import (
 	"net/http"
 
+	"github.com/Coniglio/company-map/model"
 	"github.com/labstack/echo"
+	"gopkg.in/gorp.v1"
 )
 
 // GetAlongs 沿線情報を返します
 func GetAlongs() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.JSON(http.StatusOK, "")
+		tx := c.Get("Tx").(*gorp.Transaction)
+
+		alongs, err := model.GetAlongs(tx)
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, alongs)
 	}
 }
--- model/along.go
+++ model/along.go
@@ -9,6 +9,31 @@ type Along struct {
 }
 
 // GetAlongs 沿線情報を取得します
-func GetAlongs(tx *gorp.Transaction) error {
-	return nil
+func GetAlongs(tx *gorp.Transaction) ([]Along, error) {
+
+	alongs, err := selectToAlongs(tx)
+	if err != nil {
+		return alongs, err
+	}
+
+	return alongs, nil
+}
+
+// selectToAlongs 沿線情報を検索します
+func selectToAlongs(tx *gorp.Transaction) ([]Along, error) {
+	var alongs []Along
+	_, err := tx.Select(&alongs, `
+		select
+		  id along_id,
+		  name along_name
+		from
+		  alongs
+		order by
+		  id
+	`)
+	if err != nil {
+		return alongs, err
+	}
+
+	return alongs, nil
 }
